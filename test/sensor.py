4883f019a4e08ff3a587bbf6922d259f93e51290
Marco - Andrea Buchmann <marco.andrea.buchmann@gmail.com>
2020-06-19 16:44:02 +0100

    Add tests for sensor module

---
+++ test/sensor.py
@@ -0,0 +1,125 @@
+import io
+import unittest
+import mock
+
+from bme280pi.sensor import Sensor
+
+
+class FakeSMBus:
+    def __init__(self, value):
+        self.value = value
+
+    def read_i2c_block_data(self, address, register, length, force=None):
+        return "fake_chip_id", "fake_version"
+
+
+class test_initialize_bus(unittest.TestCase):
+    def test(self):
+        # this test requires us to override the processor type and smbbus
+        known_revisions = {'0002': 0,
+                           '0003': 0,
+                           '0004': 1,
+                           '0005': 1,
+                           '0006': 1,
+                           '0007': 0,
+                           '0008': 0,
+                           '0009': 0,
+                           '000d': 1,
+                           '000e': 1,
+                           '000f': 1,
+                           '0010': 0,
+                           '0011': 1,
+                           '0012': 0,
+                           'a01041': 1,
+                           'a21041': 1,
+                           '900092': 1,
+                           '900093': 1,
+                           'a02082': 1,
+                           'a22082': 1,
+                           '9000c1': 1,
+                           'c03111': 1,
+                           'abcdef': 1,
+                           '0000': 1}
+
+        for revision in known_revisions:
+            m = mock.mock_open(read_data="\nRevision:" + revision + "\n")
+            with mock.patch('builtins.open', m):
+                with mock.patch('smbus.SMBus', FakeSMBus):
+                    s = Sensor()
+                    self.assertEqual(s.bus.value, known_revisions[revision])
+
+
+class FakeDataBus:
+    def __init__(self):
+        self.i_read = -1
+        self.data = [['fake_chip_id', 'fake_version'],
+                     [96, 110, 203, 104, 50, 0, 29, 145, 59, 215, 208, 11,
+                      232, 38, 42, 255, 249, 255, 172, 38, 10, 216, 189, 16],
+                     [75],
+                     [129, 1, 0, 16, 44, 3, 30],
+                     [76, 60, 128, 129, 49, 128, 94, 120]]
+
+    def write_byte_data(self, address, register, value, force=None):
+        pass
+
+    def read_i2c_block_data(self, address, register, length, force=None):
+        self.i_read += 1
+        return self.data[self.i_read]
+
+
+def initialize_fake_bus(*args, **kwargs):
+    return FakeDataBus()
+
+
+class test_sensor(unittest.TestCase):
+    def test(self):
+        Sensor._initialize_bus = initialize_fake_bus
+        s = Sensor()
+        self.assertEqual(s.chip_id, "fake_chip_id")
+        self.assertEqual(s.chip_version, "fake_version")
+
+        data = s.get_data()
+        self.assertLess(abs(data['temperature'] - 24.65), 1e-4)
+        self.assertLess(abs(data['pressure'] - 969.1056565652227), 1e-4)
+        self.assertLess(abs(data['humidity'] - 41.07329061361983), 1e-4)
+
+        s = Sensor()
+        temperature = s.get_temperature()
+        self.assertLess(abs(temperature - 24.65), 1e-4)
+
+        s = Sensor()
+        pressure = s.get_pressure()
+        self.assertLess(abs(pressure - 969.1056565652227), 1e-4)
+
+        s = Sensor()
+        humidity = s.get_humidity()
+        self.assertLess(abs(humidity - 41.07329061361983), 1e-4)
+
+        s = Sensor()
+        humidity = s.get_humidity(relative=False)
+        self.assertLess(abs(humidity - 0.009291279797753835), 1e-4)
+
+
+class test_print_sensor(unittest.TestCase):
+    def test(self):
+        Sensor._initialize_bus = initialize_fake_bus
+        s = Sensor()
+        self.assertEqual(s.chip_id, "fake_chip_id")
+        self.assertEqual(s.chip_version, "fake_version")
+
+        ref_message = "Temperature:  24.65 C\n" + \
+                      "Humidity:     41.07 %\n" + \
+                      "Pressure:     969.1 hPa\n"
+
+        with mock.patch("sys.stdout", new_callable=io.StringIO) as fake_out:
+            s.print_data()
+            self.assertEqual(fake_out.getvalue(), ref_message)
+
+        s = Sensor()
+        ref_message = "Temperature:  24.65 C\n" + \
+                      "Humidity:     0.009291 kg / m^3\n" + \
+                      "Pressure:     969.1 hPa\n"
+
+        with mock.patch("sys.stdout", new_callable=io.StringIO) as fake_out:
+            s.print_data(relative_humidity=False)
+            self.assertEqual(fake_out.getvalue(), ref_message)
