d866130eded02ad6e6849227cac7dade25998028
Marco - Andrea Buchmann <marco.andrea.buchmann@gmail.com>
2020-06-22 16:10:27 +0100

    Add tests for validation functions

--- test/physics.py
+++ test/physics.py
@@ -1,11 +1,39 @@
-import unittest
+from unittest import TestCase
 
-from bme280pi.physics import (pressure_function, calculate_abs_humidity,
-                              convert_pressure, convert_temperature,
+from bme280pi.physics import (validate_pressure, validate_temperature,
+                              validate_humidity, pressure_function,
+                              calculate_abs_humidity, convert_pressure,
+                              convert_temperature,
                               round_to_n_significant_digits)
 
 
-class test_pressure_function(unittest.TestCase):
+class test_validation(TestCase):
+    def test_validate_pressure(self):
+        with self.assertRaises(TypeError):
+            validate_pressure("string_input")
+        with self.assertRaises(ValueError):
+            validate_pressure(-1)
+        with self.assertRaises(ValueError):
+            validate_pressure(1200)
+
+    def test_validate_temperature(self):
+        with self.assertRaises(TypeError):
+            validate_temperature("string_input")
+        with self.assertRaises(ValueError):
+            validate_temperature(-111)
+        with self.assertRaises(ValueError):
+            validate_temperature(111)
+
+    def test_validate_humidity(self):
+        with self.assertRaises(TypeError):
+            validate_humidity("string_input")
+        with self.assertRaises(ValueError):
+            validate_humidity(-1)
+        with self.assertRaises(ValueError):
+            validate_humidity(123)
+
+
+class test_pressure_function(TestCase):
     def test(self):
         test_values = [100, 800, 850, 900, 950, 1000, 1050, 1100]
         correct_values = [1.001176,
@@ -17,20 +45,21 @@ class test_pressure_function(unittest.TestCase):
                           1.0048475238095238,
                           1.0050087272727273]
 
-        for i_test in range(len(test_values)):
-            test_result = pressure_function(test_values[i_test])
+        for i_test, test_value in enumerate(test_values):
+            test_result = pressure_function(test_value)
             correct_result = correct_values[i_test]
             self.assertLess(abs(test_result - correct_result), 1e-6)
 
-        with self.assertRaises(AssertionError):
+    def test_exceptions(self):
+        with self.assertRaises(ValueError):
             pressure_function(0)
-        with self.assertRaises(AssertionError):
+        with self.assertRaises(ValueError):
             pressure_function(1111)
-        with self.assertRaises(AssertionError):
+        with self.assertRaises(TypeError):
             pressure_function('a')
 
 
-class test_calculate_abs_humidity(unittest.TestCase):
+class test_calculate_abs_humidity(TestCase):
     def test(self):
         test_pressure = [100, 800, 850, 900, 950, 1000, 1050, 1100]
         test_temp = [-10, 0, 10, 15, 20, 25, 40, 60]
@@ -43,66 +72,62 @@ class test_calculate_abs_humidity(unittest.TestCase):
                           0.006927846890531939,
                           0.011536889704637605,
                           0.022155411985739303,
-                          0.01612715205125449,
-                          0.29632475391821494]
+                          0.01612715205125449]
 
-        for i_test in range(len(test_pressure)):
+        for i_test, correct_value in enumerate(correct_values):
             result = calculate_abs_humidity(pressure=test_pressure[i_test],
                                             temperature=test_temp[i_test],
                                             rel_humidity=test_rel[i_test])
             self.assertLess(abs(result - correct_values[i_test]), 1e-6)
 
-        # test valid ranges
-        with self.assertRaises(AssertionError):
+    def test_valid_range(self):
+        with self.assertRaises(ValueError):
             # pressure too low
             calculate_abs_humidity(pressure=0,
                                    temperature=25,
                                    rel_humidity=50)
-        with self.assertRaises(AssertionError):
+        with self.assertRaises(ValueError):
             # pressure too high
             calculate_abs_humidity(pressure=1111,
                                    temperature=25,
                                    rel_humidity=50)
-        with self.assertRaises(AssertionError):
+        with self.assertRaises(ValueError):
             # rel humidity too low
             calculate_abs_humidity(pressure=975,
                                    temperature=25,
                                    rel_humidity=-1)
-        with self.assertRaises(AssertionError):
+        with self.assertRaises(ValueError):
             # rel humidity too high
             calculate_abs_humidity(pressure=975,
                                    temperature=25,
                                    rel_humidity=110)
-        with self.assertRaises(AssertionError):
+        with self.assertRaises(ValueError):
             # temp too low
             calculate_abs_humidity(pressure=975,
                                    temperature=-101,
                                    rel_humidity=50)
-        with self.assertRaises(AssertionError):
+        with self.assertRaises(ValueError):
             # temp too high
             calculate_abs_humidity(pressure=975,
                                    temperature=101,
                                    rel_humidity=50)
 
-        # test bad arguments
-        with self.assertRaises(AssertionError):
-            # invalid argument
+    def test_bad_arguments(self):
+        with self.assertRaises(TypeError):
             calculate_abs_humidity(pressure='a',
                                    temperature=25,
                                    rel_humidity=110)
-        with self.assertRaises(AssertionError):
-            # invalid argument
+        with self.assertRaises(TypeError):
             calculate_abs_humidity(pressure=975,
                                    temperature='a',
                                    rel_humidity=110)
-        with self.assertRaises(AssertionError):
-            # invalid argument
+        with self.assertRaises(TypeError):
             calculate_abs_humidity(pressure=975,
                                    temperature=25,
                                    rel_humidity='a')
 
 
-class test_convert_pressure(unittest.TestCase):
+class test_convert_pressure(TestCase):
     def test(self):
         test_values = [100, 500, 850, 900, 950, 1000, 1050, 1099]
 
@@ -119,18 +144,19 @@ class test_convert_pressure(unittest.TestCase):
                                                      unit=unit)),
                                 1e-6)
 
-        with self.assertRaises(AssertionError):
+    def test_exceptions(self):
+        with self.assertRaises(ValueError):
             convert_pressure(-123)
-        with self.assertRaises(AssertionError):
+        with self.assertRaises(ValueError):
             convert_pressure(1234)
 
-        with self.assertRaises(AssertionError):
+        with self.assertRaises(TypeError):
             convert_pressure('a')
         with self.assertRaises(Exception):
             convert_pressure(123, 'UnknownUnit')
 
 
-class test_convert_temperature(unittest.TestCase):
+class test_convert_temperature(TestCase):
     def test(self):
         test_values = [-50, -40, 30, 20, 10, 0, 12.34, 20, 30, 40, 50, 99]
 
@@ -145,18 +171,19 @@ class test_convert_temperature(unittest.TestCase):
                                                         unit=unit)),
                                 1e-6)
 
-        with self.assertRaises(AssertionError):
+    def test_exceptions(self):
+        with self.assertRaises(ValueError):
             convert_temperature(-123)
-        with self.assertRaises(AssertionError):
+        with self.assertRaises(ValueError):
             convert_temperature(123)
 
-        with self.assertRaises(AssertionError):
+        with self.assertRaises(TypeError):
             convert_temperature('a')
         with self.assertRaises(Exception):
             convert_temperature(23, 'UnknownUnit')
 
 
-class test_round_to_n_significant_digits(unittest.TestCase):
+class test_round_to_n_significant_digits(TestCase):
     def test(self):
         multipliers = [0.001, 0.01, 0.1, 1, 10, 100, 1000]
         test_values = [1.23456789 * m for m in multipliers]
@@ -168,20 +195,21 @@ class test_round_to_n_significant_digits(unittest.TestCase):
                            5: [1.2346 * m for m in multipliers]}
 
         for n_digits in correct_results:
-            for i in range(len(test_values)):
-                test_result = round_to_n_significant_digits(test_values[i],
+            for i, test_value in enumerate(test_values):
+                test_result = round_to_n_significant_digits(test_value,
                                                             n_digits)
                 self.assertLess(abs(correct_results[n_digits][i] -
                                     test_result),
                                 1e-6)
 
-        with self.assertRaises(AssertionError):
+    def test_exceptions(self):
+        with self.assertRaises(TypeError):
             round_to_n_significant_digits('a', 2)
-        with self.assertRaises(AssertionError):
+        with self.assertRaises(TypeError):
             round_to_n_significant_digits(2, 'a')
-        with self.assertRaises(AssertionError):
+        with self.assertRaises(ValueError):
             round_to_n_significant_digits(2, 0)
-        with self.assertRaises(AssertionError):
+        with self.assertRaises(ValueError):
             round_to_n_significant_digits(2, -1)
-        with self.assertRaises(AssertionError):
+        with self.assertRaises(TypeError):
             round_to_n_significant_digits(2, 1.234)
